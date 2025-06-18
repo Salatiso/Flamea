97d0f6d5fae4f3b69449dc3e4af097348cebe45d
Salatiso <spiceinc@gmail.com>
2025-06-18 20:43:38 +0200

    style.min.css creation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './*.html',           // Scans all HTML files in the root (e.g., index.html, sidebar.html)
+    './games/*.html',     // Scans HTML files in the "games" folder
+    './training/*.html',  // Scans HTML files in the "training" folder
+    './assets/js/*.js'    // Scans JS files in the "assets/js" folder for Tailwind classes
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
