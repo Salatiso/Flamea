eaec0c5ff94e85ea3fdc203d2b6d6ff515b37f1f
Salatiso <spiceinc@gmail.com>
2025-06-18 17:47:08 +0200

    tailwind

---
+++ assets/js/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './*.html',
+        './games/*.html',
+        './training/*.html',
+        './assets/js/*.js'
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
